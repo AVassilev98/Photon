9a39a9e6253bcc6206b075dd94a2e49e8d4e68ff
Anton Vassilev <anton@Mac.attlocal.net>
2026-03-28 12:08:57 -0500

    Add mesh class to Photon

--- include/photon/photon_mesh.h
+++ include/photon/photon_mesh.h
@@ -1,17 +1,23 @@
 #pragma once
 #include "cglm/cglm.h"
+#include "foundation/span.h"
 #include "photon/photon_device.h"
 #include "photon/photon_pipeline.h"
 #include "photon/photon_status.h"
+
 
 typedef struct PhVertex {
     vec2 position;
     vec3 color;
 } PhVertex;
+FDN_SPAN_DEFINE(PhVertex, PhVertexSpan);
+FDN_SPAN_DEFINE(uint16_t, PhIndexSpan);
 
 typedef struct PhMesh {
-    PhVertex *vertices;
+    PhVertexSpan vertices;
+    PhIndexSpan indices;
     PhBuffer gpuVertexBuffer;
+    PhBuffer gpuIndexBuffer;
 } PhMesh;
 
 typedef struct PhRenderObject {
@@ -24,5 +30,5 @@ typedef struct PhRenderObjectCreateInfo {
     PhMesh     *mesh;
 } PhRenderObjectCreateInfo;
 PhStatus ph_render_object_create(PhRenderObjectCreateInfo createInfo, PhRenderObject *out);
-PhStatus ph_mesh_create(PhDeviceHandle hDevice, PhVertex *vertices, uint32_t count, PhMesh *out);
+PhStatus ph_mesh_create(PhDeviceHandle hDevice, PhVertexSpan vertices, PhIndexSpan indices, PhMesh *out);
 PhStatus ph_mesh_create_from_file(PhDeviceHandle hDevice, const char *path, PhMesh *out);
--- src/photon_mesh.c
+++ src/photon_mesh.c
@@ -13,18 +13,29 @@ PhStatus ph_render_object_create(PhRenderObjectCreateInfo createInfo, PhRenderOb
     return PH_SUCCESS;
 }
 
-PhStatus ph_mesh_create(PhDeviceHandle hDevice, PhVertex *vertices, uint32_t count, PhMesh *out)
+PhStatus ph_mesh_create(PhDeviceHandle hDevice, PhVertexSpan vertices, PhIndexSpan indices, PhMesh *out)
 {
-    PhBuffer buffer;
+    PhBuffer vertexBuffer;
+    PhBuffer indexBuffer;
+
     PH_CHECK(PH_LOG_ERROR, 
-        ph_device_buffer_create(hDevice, PH_QUEUE_TYPE_GRAPHICS_BIT | PH_QUEUE_TYPE_TRANSFER_BIT, count * sizeof(PhVertex),
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_SHARING_MODE_EXCLUSIVE, &buffer, 0));
+        ph_device_buffer_create(hDevice, PH_QUEUE_TYPE_GRAPHICS_BIT | PH_QUEUE_TYPE_TRANSFER_BIT, vertices.len * sizeof(PhVertex),
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_SHARING_MODE_EXCLUSIVE, &vertexBuffer, 0));
     PH_CHECK(PH_LOG_ERROR, 
-        ph_device_buffer_upload(hDevice, vertices, count * sizeof(PhVertex), buffer));
+        ph_device_buffer_upload(hDevice, vertices.ptr, vertices.len * sizeof(PhVertex), vertexBuffer));
+
+    PH_CHECK(PH_LOG_ERROR, 
+        ph_device_buffer_create(hDevice, PH_QUEUE_TYPE_GRAPHICS_BIT | PH_QUEUE_TYPE_TRANSFER_BIT, indices.len * sizeof(uint16_t),
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_SHARING_MODE_EXCLUSIVE, &indexBuffer, 0));
+    PH_CHECK(PH_LOG_ERROR, 
+        ph_device_buffer_upload(hDevice, indices.ptr, indices.len * sizeof(uint16_t), indexBuffer));
+
 
     *out = (PhMesh) {
         .vertices = vertices,
-        .gpuVertexBuffer = buffer
+        .gpuVertexBuffer = vertexBuffer,
+        .indices = indices,
+        .gpuIndexBuffer = indexBuffer,
     };
 
     return PH_SUCCESS;
